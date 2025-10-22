89c50246e0a768b8f81e8017817afd851ca98567
Pigeonlover <bravefoot@outlook.com>
2025-10-22 14:53:51 +0100

    Profile edit.

--- src/app/profile/[profileId]/page.jsx
+++ src/app/profile/[profileId]/page.jsx
@@ -1,18 +1,13 @@
 import { db } from "@/db";
+import auth from "../../middleware";
 
-export default function UserProfile() {
+export default async function UserProfile({ params }) {
+  const session = await auth();
+
   return (
     <>
       <div>
-        <div
-          className={`rounded-2xl bg-gradient-to-br from-red-500 to-rose-800 flex items-center justify-center text-white font-bold shadow-lg ${className}`}
-          style={{
-            width: size,
-            height: size,
-            fontSize: Math.max(24, size / 3),
-          }}
-          aria-hidden
-        >
+        <div className="first-letter:rounded-2xl bg-gradient-to-br from-red-500 to-rose-800 flex items-center justify-center text-white font-bold shadow-lg">
           {session.user.name.charAt(0).toUpperCase()}
         </div>
         <span className="text-xs text-zinc-400 mr-3">#{session.user.id}</span>{" "}
